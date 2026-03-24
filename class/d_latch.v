b6bdbfb1828603b5d8f9db7524c15b024e56f5fc
Dylan-Owen33 <dylanowen@u.boisestate.edu>
2026-03-24 16:02:42 -0600

    Document D latch functionality with comments
    Added comments explaining the functionality of the D latch.

--- class/d_latch.v
+++ class/d_latch.v
@@ -13,3 +13,14 @@ module d_latch(
   assign NotQ = ~Q;
 
 endmodule
+/*
+This is the 1-bit memory part.
+D is the value you want to save
+E is the enable/store signal
+Q is the saved value
+NotQ is just the opposite of Q
+How it works:
+When E is 1, the latch copies D into Q
+When E goes back to 0, it keeps whatever value was last stored
+NotQ is always the inverse of Q
+*/
