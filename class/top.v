8fdaaca65e0c743c339862a2a82430a09a4004ff
Dylan-Owen33 <dylanowen@u.boisestate.edu>
2026-03-24 16:05:08 -0600

    Document connections for lab switches and LEDs
    Added comments explaining the connections for switches, buttons, and LEDs in the lab.

--- class/top.v
+++ class/top.v
@@ -19,3 +19,17 @@ module top(input [15:0] sw,
   assign led[7:2] = 6'b0;
 
 endmodule
+
+/*
+This connects the board switches, button, and LEDs to both parts of the lab.
+Part 1
+sw[0] = D
+btnC = E
+led[0] = Q
+led[1] = NotQ
+Part 2
+sw[15:8] = 8-bit data input
+sw[7:6] = address
+btnC = store
+led[15:8] = selected memory output
+*/
